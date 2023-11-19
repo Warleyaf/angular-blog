2a6498e8d016a2238f10c1aba10c960fa1fcb622
Warley Afonso <warleysilva365@gmail.com>
2023-11-19 10:31:44 -0300

    Adicionando a parte final desse projeto

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,20 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
+    "id":"1",
     "title": "saiu novo filme",
-    "description": "Ola meu chapa"
+    "description": "Ola meu chapa",
+    "photoCover": "https://gifs.eco.br/wp-content/uploads/2021/09/gifs-de-anime-aesthetic-49.gif"
   },
   {
-    "id":1,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description": "Ola meu chapa"
+    "id":"2",
+    "title": "Chupisco o nildo",
+    "description": "Eai chupisco o nildo, suve?",
+    "photoCover": "https://i.pinimg.com/originals/cc/ed/5a/cced5a055436f9f0ed1ef11160ac2318.gif"
+  },
+  {
+    "id":"3",
+    "title": "DemonSlayer anunciado",
+    "description": "O anime aclamado no ocidente e principalmente no brasil foi anunciado a 4 temporada nessa tarde de domingo",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/blob_szmv.jpg"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,16 +8,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover: string = "https://i.pinimg.com/originals/cc/ed/5a/cced5a055436f9f0ed1ef11160ac2318.gif";
-  contentTitle: string = "Zenitsu o Brabo";
-  contentDescription: string = "aksjldhklasjjdaslkdjaslkdjasldjsaldjsal";
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+  private id:string | null = "0";
 
   constructor(
     private route:ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(value => console.log(value.get("id")))
+    this.route.paramMap.subscribe(value => this.id = value.get("id"))
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 
 }
